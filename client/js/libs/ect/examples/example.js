9ce0d8a96481b9728a3959df9ffd0992c4e3a0cc
Maxim Oleinyk <maxim.oleinyk@gmail.com>
2014-04-05 00:06:43 +0300

    Removed bower components and dist directory

--- client/js/libs/ect/examples/example.js
+++
@@ -1,8 +0,0 @@
-var ECT = require('./../index');
-
-var renderer = ECT({ root : __dirname + '/view', ext : '.html' });
-
-renderer.render('page', { title: 'Hello, World!' }, function(error, html) {
-	console.log(error);
-	console.log(html);
-});
